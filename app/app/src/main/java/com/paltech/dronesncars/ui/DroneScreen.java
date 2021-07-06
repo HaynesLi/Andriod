4ed66d742c0ad6fec2c9dddcb6624f7156d0a7c2
Jonas Bogenberger <bogenber@in.tum.de>
2021-07-06 15:14:16 +0200

    [Navigation][UI] removed StartScreen.java and its' layout. added button to import polygon from kml to DroneScreen, added the possibility to edit the polygon from scratch (if there are no polygon vertices yet -> you have to add at least 3 to create a new polygon)

--- app/app/src/main/java/com/paltech/dronesncars/ui/DroneScreen.java
+++ app/app/src/main/java/com/paltech/dronesncars/ui/DroneScreen.java
@@ -1,6 +1,5 @@
 package com.paltech.dronesncars.ui;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,24 +13,12 @@ import com.paltech.dronesncars.R;
 
 public class DroneScreen extends LandscapeFragment<ViewBinding, ViewModel> {
 
-    public Uri getKml_file_uri() {
-        return kml_file_uri;
-    }
-
-    private Uri kml_file_uri;
-
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-
-        kml_file_uri = null;
-        String uri_string = DroneScreenArgs.fromBundle(getArguments()).getKmlFileUri();
-        if (uri_string != null) {
-            kml_file_uri = Uri.parse(uri_string);
-        }
 
         return inflater.inflate(R.layout.fragment_drone_screen, container, false);
     }
--- app/app/src/main/java/com/paltech/dronesncars/ui/FlightMapFragment.java
+++ app/app/src/main/java/com/paltech/dronesncars/ui/FlightMapFragment.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -19,7 +21,6 @@ import org.osmdroid.events.MapEventsReceiver;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.overlay.MapEventsOverlay;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.OverlayManager;
 import org.osmdroid.views.overlay.Polygon;
 
 import java.util.ArrayList;
@@ -40,7 +41,8 @@ public class FlightMapFragment extends MapFragment {
     private List<List<Marker>> polygon_holes;
     private List<Marker> edit_route_markers;
 
-    private boolean initial_polygon_edit;
+    private final ActivityResultLauncher<String> getKML = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            this::use_kml_result);
 
 
     public FlightMapFragment() {
@@ -79,9 +81,14 @@ public class FlightMapFragment extends MapFragment {
 
         super.onViewCreated(view, savedInstanceState);
 
+        view_binding.buttonImportKml.setVisibility(View.VISIBLE);
+        view_binding.buttonPolygonEdit.setVisibility(View.VISIBLE);
+        view_binding.buttonPolygonEdit.setEnabled(true);
+        view_binding.buttonDeletePolygon.setVisibility(View.VISIBLE);
+
         view_binding.buttonPolygonEdit.setText("Edit Polygon");
 
-        getArgsFromParent();
+        //getArgsFromParent();
 
     }
 
@@ -117,9 +124,7 @@ public class FlightMapFragment extends MapFragment {
 
     @Override
     protected void different_inits() {
-        initial_polygon_edit = false;
         edit_route_markers = null;
-
     }
 
     @Override
@@ -138,31 +143,12 @@ public class FlightMapFragment extends MapFragment {
     }
 
 
-    private void getArgsFromParent() {
-        DroneScreen parentDroneScreen = (DroneScreen) getParentFragment();
-        if (parentDroneScreen != null) {
-            Uri kml_file_uri = parentDroneScreen.getKml_file_uri();
-            if (kml_file_uri != null) {
-                parseKMLFile(kml_file_uri);
-            } else {
-                initial_polygon_edit();
-            }
-        } else {
-            initial_polygon_edit();
-        }
-    }
-
-    private void initial_polygon_edit() {
-        initial_polygon_edit = true;
-        set_center_to_last_location();
-        start_polygon_edit();
-    }
-
     private void start_polygon_edit() {
         view_binding.buttonExportPolygonToKml.setVisibility(View.INVISIBLE);
         view_binding.buttonEditRoute.setEnabled(false);
         view_binding.buttonPolygonEdit.setEnabled(true);
         view_binding.buttonPolygonEdit.setText("stop edit");
+        view_binding.buttonDeletePolygon.setEnabled(false);
 
         view_binding.map.invalidate();
         current_state = VIEW_STATE.EDIT_POLYGON;
@@ -178,9 +164,10 @@ public class FlightMapFragment extends MapFragment {
         polygon_vertices = null;
         polygon_holes = null;
         view_binding.buttonPolygonEdit.setText("edit polygon");
-        view_binding.buttonPolygonEdit.setEnabled(false);
+        view_binding.buttonPolygonEdit.setEnabled(true);
         view_binding.buttonEditRoute.setEnabled(true);
         view_binding.buttonExportPolygonToKml.setVisibility(View.VISIBLE);
+        view_binding.buttonDeletePolygon.setEnabled(true);
         view_binding.map.invalidate();
         current_state = VIEW_STATE.NONE;
     }
@@ -207,47 +194,58 @@ public class FlightMapFragment extends MapFragment {
         super.set_click_listeners();
         view_binding.buttonPolygonEdit.setOnClickListener(v -> {
             if (current_state == VIEW_STATE.EDIT_POLYGON) {
-                Polygon self_selected_polygon = new Polygon();
-                if (polygon_vertices != null) {
+                if (polygon_vertices != null && polygon_vertices.size() >= 3) {
+                    Polygon self_selected_polygon = new Polygon();
                     for (Marker self_set_marker : polygon_vertices) {
                         self_selected_polygon.addPoint(self_set_marker.getPosition());
                     }
-                }
-                List<List<GeoPoint>> new_holes = new ArrayList<>();
-                if (polygon_holes != null) {
-                    for (List<Marker> hole: polygon_holes) {
-                        List<GeoPoint> current_hole = new ArrayList<>();
-                        for (Marker self_set_marker: hole) {
-                            current_hole.add(self_set_marker.getPosition());
+                    List<List<GeoPoint>> new_holes = new ArrayList<>();
+                    if (polygon_holes != null) {
+                        for (List<Marker> hole : polygon_holes) {
+                            List<GeoPoint> current_hole = new ArrayList<>();
+                            for (Marker self_set_marker : hole) {
+                                current_hole.add(self_set_marker.getPosition());
+                            }
+                            new_holes.add(current_hole);
                         }
-                        new_holes.add(current_hole);
                     }
+                    self_selected_polygon.setHoles(new_holes);
+
+                    view_model.setPolygon(self_selected_polygon);
+                } else {
+                    view_model.clearPolygon();
                 }
-                self_selected_polygon.setHoles(new_holes);
 
                 set_marker_unselected(true);
-
                 stop_polygon_edit();
                 view_binding.buttonAddMarker.setVisibility(View.INVISIBLE);
                 view_binding.buttonDeleteMarker.setVisibility(View.INVISIBLE);
-                initial_polygon_edit = false;
-                view_model.setPolygon(self_selected_polygon);
-            } else if (current_state == VIEW_STATE.NONE && polygon_vertices != null) {
+
+            } else if (current_state == VIEW_STATE.NONE) {
                 view_binding.buttonAddMarker.setVisibility(View.VISIBLE);
                 view_binding.buttonDeleteMarker.setVisibility(View.VISIBLE);
 
 
                 start_polygon_edit();
-
-                view_binding.map.getOverlayManager().addAll(polygon_vertices);
-                if (polygon_holes != null) {
-                    for (List<Marker> hole : polygon_holes) {
-                        view_binding.map.getOverlayManager().addAll(hole);
+                if (polygon_vertices != null && !polygon_vertices.isEmpty()) {
+                    view_binding.map.getOverlayManager().addAll(polygon_vertices);
+                    if (polygon_holes != null) {
+                        for (List<Marker> hole : polygon_holes) {
+                            view_binding.map.getOverlayManager().addAll(hole);
+                        }
                     }
                 }
 
                 view_binding.map.invalidate();
             }
+        });
+
+        view_binding.buttonImportKml.setOnClickListener(v -> getKML.launch("application/vnd.google-earth.kml+xml"));
+
+        view_binding.buttonDeletePolygon.setOnClickListener(v -> {
+            view_model.clearPolygon();
+            polygon_vertices = null;
+            polygon_holes = null;
         });
     }
 
@@ -260,35 +258,8 @@ public class FlightMapFragment extends MapFragment {
     @Override
     protected void configureMap() {
         super.configureMap();
-        OverlayManager overlay_manager = view_binding.map.getOverlayManager();
-        overlay_manager.removeAll(overlay_manager.overlays());
 
-        view_model.clearPolygon();
         view_model.clearSelectablePolygons();
-
-        final MapEventsReceiver map_events_receiver = new MapEventsReceiver() {
-            @Override
-            public boolean singleTapConfirmedHelper(GeoPoint p) {
-                if (current_state == VIEW_STATE.EDIT_POLYGON && initial_polygon_edit) {
-                    Marker tmp_marker = build_edit_marker(p, false, true);
-                    if (polygon_vertices == null) {
-                        polygon_vertices = new ArrayList<>();
-                    }
-                    polygon_vertices.add(tmp_marker);
-                    view_binding.map.getOverlayManager().add(tmp_marker);
-                    view_binding.map.invalidate();
-                }
-
-                return current_state == VIEW_STATE.EDIT_POLYGON && initial_polygon_edit;
-            }
-
-            @Override
-            public boolean longPressHelper(GeoPoint p) {
-                return false;
-            }
-        };
-
-        view_binding.map.getOverlays().add(new MapEventsOverlay(map_events_receiver));
         view_binding.map.invalidate();
 
     }
@@ -353,8 +324,17 @@ public class FlightMapFragment extends MapFragment {
 
     @Override
     protected void add_marker_polygon_edit() {
+        if (polygon_vertices == null) {
+            polygon_vertices = new ArrayList<>();
+        }
             Marker new_marker = build_edit_marker((GeoPoint) view_binding.map.getMapCenter(), true, false);
-            if (polygon_vertices.contains(selected_marker)) {
+            if (selected_marker == null && (polygon_vertices == null || polygon_vertices.isEmpty())) {
+                if (polygon_vertices == null) {
+                    polygon_vertices = new ArrayList<>();
+                }
+                polygon_vertices.add(new_marker);
+            }
+            else if (polygon_vertices.contains(selected_marker)) {
                 polygon_vertices = insert_marker_at_index(polygon_vertices, polygon_vertices.indexOf(selected_marker), new_marker);
             } else {
                 for (int i = 0; i < polygon_holes.size(); i++) {
@@ -426,5 +406,13 @@ public class FlightMapFragment extends MapFragment {
         view_binding.map.getOverlayManager().add(new_marker);
         view_binding.map.invalidate();
         changed_during_edit = true;
+    }
+
+    // TODO is this necessary? or is it possible to just pass the Uri directly to parseKMLFile(...),
+    //  (no null-check required?)
+    private void use_kml_result(Uri kml_file_uri) {
+        if (kml_file_uri != null) {
+            parseKMLFile(kml_file_uri);
+        }
     }
 }
